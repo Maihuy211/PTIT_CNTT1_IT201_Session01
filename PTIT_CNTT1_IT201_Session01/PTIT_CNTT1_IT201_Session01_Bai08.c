457684bd053529fa619d7cfbda1db55dd8b8943f
Huy <maihuylc@gmail.com>
2025-06-19 20:26:49 +0700

    session02

--- PTIT_CNTT1_IT201_Session01/PTIT_CNTT1_IT201_Session01_Bai08.c
+++ PTIT_CNTT1_IT201_Session01/PTIT_CNTT1_IT201_Session01_Bai08.c
@@ -4,9 +4,10 @@ int main(){
     int arr[]={2,3,4,5,6,7,7,5,4,7};
     int max=arr[0];
     int max_count = 0 ;
-    for(int i = 0 ; i < n ; i++){
+    int i,j;
+    for(i = 0 ; i < n ; i++){
         int count = 0 ;
-        for(int j = 0 ; j < n ; j++){
+        for(j = 0 ; j < n ; j++){
             if(arr[i]==arr[j]){
                 count++;
             }
